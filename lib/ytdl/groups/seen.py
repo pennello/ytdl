991a40fd1036339ec18eb94cc314d31543bceb71
Chris Pennello <ytdl@chrispennello.com>
2015-04-11 20:04:07 -0700

    Ability to list seen for specific channel.

--- lib/ytdl/groups/seen.py
+++ lib/ytdl/groups/seen.py
@@ -22,6 +22,7 @@ class Seen(DbGroup):
     seen_commands = seen.add_subparsers(dest='command',metavar='command')
     descr = 'List seen state.'
     ls = seen_commands.add_parser('ls',description=descr,help=descr)
+    ls.add_argument('chid',nargs='?',help='specific channel id')
     descr = 'Clear entirety of seen state.'
     clear = seen_commands.add_parser('clear',description=descr,help=descr)
 
@@ -33,7 +34,12 @@ class Seen(DbGroup):
   def ls(self,args):
     seen = self.read()
     if seen is None: return 127
-    for chid,vids in seen.iteritems():
-      self.out('%s %s' % (chid,self.short(vids)))
+    if args.chid is not None:
+      vids = seen.get(args.chid)
+      if vids is None: return 127
+      for vid in vids: self.out(vid)
+    else:
+      for chid,vids in seen.iteritems():
+        self.out('%s %s' % (chid,self.short(vids)))
 
   def clear(self,args): os.unlink(self.dbpath())
