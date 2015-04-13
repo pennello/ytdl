ef00c79ff2d854eda9a752601258dc163289d240
Chris Pennello <ytdl@chrispennello.com>
2015-04-12 17:14:26 -0700

    Util log,error methods take generic objects.

--- lib/ytdl/util.py
+++ lib/ytdl/util.py
@@ -13,16 +13,17 @@ def segment(itr,n):
       r = ()
   if r: yield r
 
-def log(msg):
-  sys.stderr.write(msg + '\n')
-  sys.stderr.flush()
+def write(fobj,x):
+  fobj.write('%s\n' % x)
+  fobj.flush()
+
+def log(x): write(sys.stderr,x)
+
+def out(x=''):
+  if isinstance(x, unicode): x = x.encode('utf8')
+  write(sys.stdout,x)
 
 def makedirs(name,mode=0o777,exist_ok=False):
   try: os.makedirs(name,mode)
   except os.error,e:
     if not exist_ok or e.errno != errno.EEXIST: raise
-
-def out(s=''):
-  if isinstance(s, unicode): s = s.encode('utf8')
-  sys.stdout.write(s + '\n')
-  sys.stdout.flush()
